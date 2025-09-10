3fbf9d381ceb6cf0e8584e017d243e39e4ce5c91
Emanuel Johansson <emanueljohanss@gmail.com>
2025-09-10 21:15:06 +0000

    Add nvim config, settings, one keymap

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # hypracrh
 1. Install omarchy
 2. run install script.
+
+# reqs
+- nvim >0.12
--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,1 +1,29 @@
+--- Settings ---
+
 vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.wrap = false
+
+vim.opt.swapfile = false -- dont use swapfile
+ 
+
+-- TAB WIDTH
+local tabWidth = 2
+
+vim.opt.softtabstop = tabWidth -- use spaces in place of tabs
+vim.opt.shiftwidth = tabWidth -- << >> tabWidth
+vim.opt.tabstop = tabWidth -- how wide is tabs displayed?
+vim.opt.expandtab = true --insert spaces instead of tabs
+
+vim.opt.scrolloff = 30 -- alwas leaves 30 lines when scroll up and down
+
+--- KEY MAP ---
+vim.g.mapleader = " "
+vim.keymap.set('n', '<leader>ee', ':Ex<CR>')
+
+
+
+
+
+
